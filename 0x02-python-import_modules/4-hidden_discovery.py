8f4812842281c07a3619d35a189acd282181c37b
Yohannes Getu <61448882+YohannesGetu@users.noreply.github.com>
2021-09-09 19:00:20 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def principal():
+    for i in dir(hidden_4):
+        if not (i[0] == '_' and i[1] == '_'):
+            print(i)
+
 if __name__ == "__main__":
-    import hidden_4
-    for i in range(0, len(dir(hidden_4))):
-        if dir(hidden_4)[i][0:2] != '__':
-            print(dir(hidden_4)[i])
+    principal()
