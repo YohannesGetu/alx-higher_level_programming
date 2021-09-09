5bf57839328ddb591d780587d7bf9e820dccedfe
Yohannes Getu <61448882+YohannesGetu@users.noreply.github.com>
2021-09-09 18:58:56 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    count = len(sys.argv) - 1
-    print("{:d} ".format(count), end="")
-    if count == 0:
-        print("arguments.")
+from sys import argv
+
+
+def principal():
+    print('{} argument'.format(len(argv) - 1), end='')
+    if len(argv) == 1:
+        print('s.')
+    elif len(argv) == 2:
+        print(':')
     else:
-        if count == 1:
-            print("argument:".format(count))
-        else:
-            print("arguments:".format(count))
-        for i in range(1, len(sys.argv)):
-            print("{:d}: {}".format(i, sys.argv[i]))
+        print('s:')
+    for i in range(1, len(argv)):
+        print('{}: {}'.format(i, argv[i]))
+if __name__ == "__main__":
+    principal()
