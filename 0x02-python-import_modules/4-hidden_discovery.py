7e5d951404f979188e115f149a660e68f24a2242
Yohannes Getu <61448882+YohannesGetu@users.noreply.github.com>
2021-09-09 18:54:34 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "__":
-            print("{:s}".format(s))
+    for i in range(0, len(dir(hidden_4))):
+        if dir(hidden_4)[i][0:2] != '__':
+            print(dir(hidden_4)[i])
